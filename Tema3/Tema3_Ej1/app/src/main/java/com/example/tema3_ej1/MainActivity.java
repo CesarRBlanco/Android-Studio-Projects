f83e4b59b1f3600b4613d2f86c17fbd1563f6296
Cesar <white.h0und.f0x@gmail.com>
2019-10-15 13:56:13 +0200

    14:00 - [15/10/19]

--- Tema3/Tema3_Ej1/app/src/main/java/com/example/tema3_ej1/MainActivity.java
+++ Tema3/Tema3_Ej1/app/src/main/java/com/example/tema3_ej1/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.tema3_ej1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final  EditText editName=findViewById(R.id.editText);
+        Button btnSend=findViewById(R.id.button);
+        final Intent intent=new Intent(MainActivity.this, Secundaria.class);
+        intent.putExtra("NAME", editName.getText().toString());
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
