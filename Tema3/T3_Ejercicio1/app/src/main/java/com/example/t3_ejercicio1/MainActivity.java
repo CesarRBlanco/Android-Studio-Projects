a7c56abc4df93391b8c60aed4114bfb873105e5d
Cesar <white.h0und.f0x@gmail.com>
2019-10-16 10:35:35 +0200

    10:35 - [16/10/19]

--- Tema3/T3_Ejercicio1/app/src/main/java/com/example/t3_ejercicio1/MainActivity.java
+++ Tema3/T3_Ejercicio1/app/src/main/java/com/example/t3_ejercicio1/MainActivity.java
@@ -4,25 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int MIREQUESTCODE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final EditText editName=findViewById(R.id.editText);
+
         Button btnSend=findViewById(R.id.button);
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Intent intent=new Intent(MainActivity.this, Secundaria.class);
                 intent.putExtra("NAME", editName.getText().toString());
-                startActivity(intent);
+
+                startActivityForResult(intent, MIREQUESTCODE);
             }
         });
+
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final TextView txt =findViewById(R.id.txtView);
+        if (requestCode == MIREQUESTCODE && resultCode == RESULT_OK){
+            switch((int)data.getFloatExtra("ESTADO", 0f)){
+                case 0:
+                    txt.setText("Por los suelos"); break;
+                case 1:
+                    txt.setText("De bajón"); break;
+                case 2:
+                    txt.setText("Triste"); break;
+                case 3:
+                    txt.setText("Normal"); break;
+                case 4:
+                    txt.setText("Contento"); break;
+                case 5:
+                    txt.setText("Feliz"); break;
+            }
+        }
+    }
+
 }
--- Tema3/T3_Ejercicio1/app/src/main/java/com/example/t3_ejercicio1/Secundaria.java
+++ Tema3/T3_Ejercicio1/app/src/main/java/com/example/t3_ejercicio1/Secundaria.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class Secundaria extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    final RatingBar ratingBar=(RatingBar)findViewById(R.id.ratingBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secundaria);
         Intent ite=getIntent();
         final TextView nombre=findViewById(R.id.textViewMensaje);
         nombre.setText(("Hello "+ite.getStringExtra("NAME")));
+        final Button btnVolver=(Button)findViewById(R.id.btnVolver);
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSec=new Intent();
+                intentSec.putExtra("ESTADO", ratingBar.getRating());
+                setResult(RESULT_OK, intentSec);
+                finish();
+            }
+        });
     }
+
 }
